a2c858130f433d9a951f08f8552561f9a83aa856
Lena Zhang <lena.zhang@tpisoftware.com>
2022-12-05 12:45:48 +0800

    add link to the github page of MyCart

--- src/components/Layout/MainHeader.js
+++ src/components/Layout/MainHeader.js
@@ -1,17 +1,27 @@
 import CartButton from '../Cart/CartButton';
 import classes from './MainHeader.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const MainHeader = (props) => {
+
+  const Url = 'https://github.com/leyulunna/TheCart';
+
   return (
     <header className={classes.header}>
       <h1>TheCart</h1>
       <nav>
-        <ul>
-          <li>
-            <CartButton />
-          </li>
-        </ul>
-      </nav>
+          <ul>
+            <li>
+              <CartButton />
+            </li>
+            <li>
+              <div className={classes.font}>
+                <span><a href={Url} target="_blank" rel="noreferrer">Visit <FontAwesomeIcon icon={faGithub} /> to Learn More</a></span>
+              </div>
+            </li>
+          </ul>
+        </nav>
     </header>
   );
 };
